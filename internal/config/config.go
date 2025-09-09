e2b4e593ba89c612df948115dfeee162cb6d0995
MrShitFox <qwert2006gleb@gmail.com>
2025-09-09 22:21:09 +0300

    Add Apache stealth mode support and update configuration options

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,30 +12,28 @@ import (
 type StealthMode string
 
 const (
-	StealthNone  StealthMode = "none"
-	StealthNginx StealthMode = "nginx"
+	StealthNone   StealthMode = "none"
+	StealthNginx  StealthMode = "nginx"
+	StealthApache StealthMode = "apache" // <-- ДОБАВЛЯЕМ НОВЫЙ РЕЖИМ
 )
 
 // Config хранит все конфигурационные параметры.
 type Config struct {
 	Domain      string
 	StealthMode StealthMode
-	// В будущем можно добавить Email для Let's Encrypt, порты и т.д.
 }
 
 // New создает новую конфигурацию, считывая флаги и переменные окружения.
-// Флаги имеют приоритет над переменными окружения.
 func New() *Config {
 	cfg := &Config{}
 
 	var domain, stealthMode string
 
-	// 1. Определяем флаги
 	flag.StringVar(&domain, "domain", "", "Domain for the TLS certificate (required).")
-	flag.StringVar(&stealthMode, "stealth-mode", "nginx", "Stealth mode: 'none' or 'nginx'.")
+	// Изменяем значение по умолчанию, чтобы показать все опции в --help
+	flag.StringVar(&stealthMode, "stealth-mode", "nginx", "Stealth mode: 'none', 'nginx', or 'apache'.")
 	flag.Parse()
 
-	// 2. Если флаги не установлены, читаем переменные окружения
 	if domain == "" {
 		domain = os.Getenv("DOMAIN")
 	}
@@ -43,19 +41,21 @@ func New() *Config {
 		stealthMode = os.Getenv("STEALTH_MODE")
 	}
 
-	// 3. Валидация
 	if domain == "" {
 		log.Fatal("Domain is required. Set it with -domain flag or DOMAIN environment variable.")
 	}
 	cfg.Domain = domain
 
+	// --- ОБНОВЛЯЕМ ЛОГИКУ ВЫБОРА РЕЖИМА ---
 	switch strings.ToLower(stealthMode) {
 	case "nginx":
 		cfg.StealthMode = StealthNginx
+	case "apache":
+		cfg.StealthMode = StealthApache
 	case "none":
 		cfg.StealthMode = StealthNone
 	default:
-		log.Fatalf("Invalid stealth mode: %s. Use 'none' or 'nginx'.", stealthMode)
+		log.Fatalf("Invalid stealth mode: %s. Use 'none', 'nginx', or 'apache'.", stealthMode)
 	}
 
 	return cfg
---
+++ internal/stealth/apache.go
@@ -0,0 +1,64 @@
+// Package stealth предоставляет модули для маскировки под различные веб-серверы.
+package stealth
+
+import (
+	"fmt"
+	"time"
+)
+
+// Стандартная страница-заглушка Apache2 на Ubuntu
+const apacheHTMLBody = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Apache2 Ubuntu Default Page: It works</title>
+    <style type="text/css" media="screen">
+      * {
+        margin: 0px 0px 0px 0px;
+        padding: 0px 0px 0px 0px;
+      }
+      body, html {
+        padding: 3px 3px 3px 3px;
+        background-color: #D8DBE2;
+        font-family: Verdana, sans-serif;
+        font-size: 11pt;
+      }
+    </style>
+  </head>
+  <body>
+    <div style="margin-left: auto; margin-right: auto; width: 760px; text-align: left;">
+      <p style="text-align: center;">
+        <b><span style="font-size: 14pt;">Apache2 Ubuntu Default Page</span></b>
+      </p>
+	  <p>
+	    This is the default welcome page used to test the correct 
+	    operation of the Apache2 server after installation on Ubuntu systems.
+	  </p>
+    </div>
+  </body>
+</html>`
+
+// GetApacheResponse генерирует полный HTTP-ответ, имитирующий стандартный Apache.
+func GetApacheResponse() []byte {
+	date := time.Now().UTC().Format(time.RFC1123)
+
+	headers := fmt.Sprintf(
+		"HTTP/1.1 200 OK\r\n"+
+			"Date: %s\r\n"+
+			"Server: Apache/2.4.41 (Ubuntu)\r\n"+
+			"Last-Modified: Tue, 01 Sep 2025 12:00:00 GMT\r\n"+
+			"ETag: \"2d-4e9a49938b880\"\r\n"+
+			"Accept-Ranges: bytes\r\n"+
+			"Content-Length: %d\r\n"+
+			"Vary: Accept-Encoding\r\n"+
+			"Content-Type: text/html\r\n"+
+			"Connection: close\r\n"+
+			"\r\n",
+		date,
+		len(apacheHTMLBody),
+	)
+
+	fullResponse := headers + apacheHTMLBody
+
+	return []byte(fullResponse)
+}
